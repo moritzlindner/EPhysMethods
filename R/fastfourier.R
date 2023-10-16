69f6e554e29c2c8aa81c54b4db0ce11aad291edd
MoritzLindner <moritz.lindner@uni-marburg.de>
2023-10-16 05:48:51 +0200

    1

---
+++ R/fastfourier.R
@@ -0,0 +1,43 @@
+' @param x Numeric vector or matrix of signal data.
+#' @param samp.freq Numeric scalar, the sampling frequency in Hertz.
+#' @param ... Additional arguments to pass to \code{\link[base]{fft}}.
+#'
+#' @return A data frame with two columns: 'fur' representing the Fourier
+#' coefficients and 'freq' representing the corresponding frequencies.
+#'
+#' @examples
+#' fft_result <- fastfourier(signal, samp.freq = 1000)
+#'
+#' @export
+fastfourier <- function(x, samp.freq, ...) {
+  if(!is.null(dim(x))){
+    f<-apply(x,2,funciton(y){
+      fastfourier.core(y, samp.freq, ...)
+    })
+  }else{
+    f<-fastfourier.core(x, samp.freq, ...)
+  }
+}
+
+#' Internal function to compute the FFT of a signal.
+#'
+#' @param x Numeric vector of signal data.
+#' @param samp.freq Numeric scalar, the sampling frequency in Hertz.
+#'
+#' @return A data frame with 'fur' and 'freq' columns.
+#'
+#' @importFrom stats fft
+#' @importFrom units units
+#'
+#' @noRd
+fastfourier.core<-function(x, samp.freq, ...) {
+  if("units" %in% class(x)){
+    units(x)<-NULL
+  }
+  N <- length(x)
+  fk <- fft(x)
+  fk <- fk[2:length(fk) / 2 + 1]
+  fk <- 2 * fk[seq(1, length(fk), by = 2)] / N
+  freq <- (1:(length(fk))) * samp.freq / (2 * length(fk))
+  return(data.frame(fur = fk, freq = freq))
+}
--- hello.R
+++ hello.R
@@ -3,5 +3,3 @@ install.packages("/home/moritz/Dokumente/R/ERGtools2",repos=NULL)
 library(ERGtools2)
 Sys.unsetenv("GITHUB_PAT")
 remotes::install_github("moritzlindner/ERGtools2")
-credentials::set_github_pat("YourPAT")
-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
